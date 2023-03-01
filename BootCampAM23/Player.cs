75320eb063e41a3dce632c3e8085e14b08165c29
MUKILTEO\1714222 <MUKILTEO\1714222>
2023-03-01 09:52:36 -0800

    Add project files.

---
+++ BootCampAM23/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCampAM23
+{
+    public class Player
+    {
+        //Properties
+        public int ID { get; set; } //Assign 0 to the actual player... all others are monsters!
+        public string Name { get; set; }
+        public int Health { get; set; }
+        //Jim is asking you to customize this... ArmorSlots, Inventory, Emotes, HasMagic, Class, Gold, XP, Speed, Upgrades
+        
+        //Constructors...
+        public Player()
+        {
+
+        }
+
+        public Player(int iD, string name, int health)
+        {
+            ID = iD;
+            Name = name;
+            Health = health;
+        }
+    }
+}
---
+++ BootCampAM23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BootCampAM23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Storing.DemoVariables();
+            //Storing.ReadingInput();
+            //Storing.UsingArrays();
+
+            //Operations.Arithmetic();
+            //Operations.Comparisons();
+            //Operations.Logic();
+            //Operations.Ternary();
+
+            //Statements.BranchingIf();
+            //Statements.Switching();
+            //Statements.ForLoop();
+            //Statements.WhileLoops();
+            //Statements.ForEachLoops();
+
+            MiniDC.StartDC();
+
+            //Console.WriteLine("Hello World!!!");
+            Console.WriteLine("<Press Any Key to Quit!>");
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
